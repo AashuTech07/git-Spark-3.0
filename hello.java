46ee9729cc2ea0d362ef5022f1d2d75e1a617432
AashuTech07 <ashutoshya903@gmail.com>
2025-04-15 12:39:53 +0530

    change in spark file

--- hello.java
+++ hello.java
@@ -2,4 +2,7 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
+    public static void greet() {
+        System.out.println("Hello, Java!");
+    }
 }
