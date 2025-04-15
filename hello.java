d71896d45d0c4c46fa3965075a985c27c471a56d
AashuTech07 <ashutoshya903@gmail.com>
2025-04-15 13:52:22 +0530

    change again

--- hello.java
+++ hello.java
@@ -2,7 +2,7 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
-    public static void greet() {
+    public static void keseho() {
         System.out.println("Hello, Java!");
     }
 }
