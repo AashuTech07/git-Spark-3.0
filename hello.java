86f9a091f4b3f25bbd366eaab40bc30533bf469f
AashuTech07 <ashutoshya903@gmail.com>
2025-04-15 14:35:26 +0530

    Notification Update

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 public class hello {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, Aashutosh Yadav!");
     }
     public static void keseho() {
         System.out.println("Hello, Java!");
